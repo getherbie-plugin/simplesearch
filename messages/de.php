defa836449748e158d9906d842d1cc4f9f3cf8d8
Thomas Breuss <thomasbreuss@gmx.ch>
2015-02-14 13:56:12 +0100

    Add translations

---
+++ messages/de.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'No results found for {query}.' => 'Keine Ergebnisse für \'{query}\' gefunden.',
+    'Please enter a search term.' => 'Bitte geben Sie einen Suchbegriff ein.',
+    'Search' => 'Suchen',
+    '{count} results found.' => '{count} Ergebnisse gefunden.',
+];
