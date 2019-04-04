d71a39a64a94c7001f2059fc295c83056d252339
tebe <tbreuss@users.noreply.github.com>
2019-04-04 06:52:25 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,9 @@
 <?php
 
 return [
+    'apiVersion' => 2,
+    'pluginName' => 'simplesearch',
+    'pluginPath' => __DIR__,
     'formTemplate' => '@plugin/simplesearch/templates/form.twig',
     'resultsTemplate' => '@plugin/simplesearch/templates/results.twig',
     'usePageCache' => false
