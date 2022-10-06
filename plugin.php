7e9f99e62341d0b906cdb055091e0f5dc2938474
tbreuss <thomasbreuss@gmx.ch>
2022-10-06 22:16:53 +0200

    feat: make it compatible with Herbie CMS 2.0

--- plugin.php
+++ plugin.php
@@ -2,85 +2,55 @@
 
 use herbie\Config;
 use herbie\Environment;
-use herbie\Event;
 use herbie\PageItem;
 use herbie\Plugin;
 use herbie\PageRepositoryInterface;
 use herbie\TwigRenderer;
-use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
-use Psr\Http\Server\MiddlewareInterface;
-use Psr\Http\Server\RequestHandlerInterface;
+use Twig\TwigFunction;
 
-class GetherbiePluginSimplesearch extends Plugin implements MiddlewareInterface
+class GetherbiePluginSimplesearch extends Plugin
 {
-    private $config;
-    private $environment;
-    private $pageRepository;
-    private $twigRenderer;
-    private $request;
+    private Config $config;
+    private Environment $environment;
+    private PageRepositoryInterface $pageRepository;
+    private ServerRequestInterface $request;
+    private TwigRenderer $twigRenderer;
 
-    /**
-     * SimplesearchPlugin constructor.
-     * @param Config $config
-     * @param Environment $environment
-     * @param PageRepositoryInterface $pageRepository
-     * @param TwigRenderer $twigRenderer
-     */
     public function __construct(
         Config $config,
         Environment $environment,
         PageRepositoryInterface $pageRepository,
+        ServerRequestInterface $request,
         TwigRenderer $twigRenderer
     ) {
         $this->config = $config;
         $this->environment = $environment;
         $this->pageRepository = $pageRepository;
+        $this->request = $request;
         $this->twigRenderer = $twigRenderer;
     }
-    
-    /**
-     * @param ServerRequestInterface $request
-     * @param RequestHandlerInterface $handler
-     * @return ResponseInterface
-     */
-    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
-    {
-        $this->request = $request;
-        return $handler->handle($request);
-    }
 
-    public function events(): array
+    public function twigFunctions(): array
     {
         return [
-            ['onTwigInitialized', [$this, 'onTwigInitialized']]
+            new TwigFunction('simplesearch_results', [$this, 'results'], ['is_safe' => ['html']]),
+            new TwigFunction('simplesearch_form', [$this, 'form'], ['is_safe' => ['html']]),
         ];
     }
 
     /**
-     * @param Event $event
-     */
-    public function onTwigInitialized(Event $event)
-    {
-        /** @var TwigRenderer $twig */
-        $twig = $event->getTarget();
-        $twig->addFunction(
-            new \Twig\TwigFunction('simplesearch_results', [$this, 'results'], ['is_safe' => ['html']])
-        );
-        $twig->addFunction(
-            new \Twig\TwigFunction('simplesearch_form', [$this, 'form'], ['is_safe' => ['html']])
-        );
-    }
-
-    /**
      * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
      */
     public function form(): string
     {
-        $name = $this->config->get(
-            'plugins.config.simplesearch.formTemplate',
-            '@plugin/simplesearch/templates/form.twig'
-        );
+        $name = $this->config->get('plugins.simplesearch.formTemplate', function () {
+            return $this->getComposerOrLocalTemplatesPath('form.twig');
+        });
+
         $action = $this->environment->getPathInfo();
         $queryParams = $this->request->getQueryParams();
         return $this->twigRenderer->renderTemplate($name, [
@@ -91,16 +61,20 @@ class GetherbiePluginSimplesearch extends Plugin implements MiddlewareInterface
 
     /**
      * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
      */
     public function results(): string
     {
         $queryParams = $this->request->getQueryParams();
         $query = $queryParams['query'] ?? '';
         $results = $this->search($query);
-        $name = $this->config->get(
-            'plugins.config.simplesearch.resultsTemplate',
-            '@plugin/simplesearch/templates/results.twig'
-        );
+
+        $name = $this->config->get('plugins.simplesearch.resultsTemplate', function () {
+            return $this->getComposerOrLocalTemplatesPath('results.twig');
+        });
+
         return $this->twigRenderer->renderTemplate($name, [
             'query' => $query,
             'results' => $results,
@@ -122,19 +96,14 @@ class GetherbiePluginSimplesearch extends Plugin implements MiddlewareInterface
             return [$title, $content];
         }
 
-        // @see herbie\Application::renderPage()
-        $cacheId = 'page-' . $item->route;
-        $content = $this->pageCache->get($cacheId);
-        if ($content !== null) {
-            return [strip_tags($content)];
-        }
-
+        // TODO load and return cached page content
         return [];
     }
 
     /**
      * @param string $query
      * @return array
+     * @throws Exception
      */
     private function search(string $query): array
     {
@@ -146,13 +115,13 @@ class GetherbiePluginSimplesearch extends Plugin implements MiddlewareInterface
         $max = 100;
         $results = [];
 
-        $usePageCache = $this->config->get('plugins.config.simplesearch.usePageCache', false);
+        $usePageCache = $this->config->get('plugins.simplesearch.usePageCache', false);
 
         $appendIterator = new \AppendIterator();
         $appendIterator->append($this->pageRepository->findAll()->getIterator());
 
         foreach ($appendIterator as $item) {
-            if ($i>$max || empty($item->title) || !empty($item->no_search)) {
+            if (($i > $max) || empty($item->title) || !empty($item->no_search)) {
                 continue;
             }
             $data = $this->loadPageData($item, $usePageCache);
@@ -181,5 +150,14 @@ class GetherbiePluginSimplesearch extends Plugin implements MiddlewareInterface
             }
         }
         return false;
+    }
+
+    private function getComposerOrLocalTemplatesPath(string $name): string
+    {
+        $composerPath = '@vendor/getherbie/plugin-simplesearch/templates/' . $name;
+        $localPath = '@plugin/plugin-simplesearch/templates/' . $name;
+        return $this->twigRenderer->getTwigEnvironment()->getLoader()->exists($composerPath)
+            ? $composerPath
+            : $localPath;
     }
 }
